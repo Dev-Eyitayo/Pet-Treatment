deed44709e88efda8b5387759acb21cf3682ce9e
Ezekiel Eyitayo <ezekieleyitayo2020@gmail.com>
2025-07-22 11:08:30 +0100

    Fixed passdowrd reset func

--- pet-backend/Curapets/settings.py
+++ pet-backend/Curapets/settings.py
@@ -89,6 +89,19 @@ CSRF_COOKIE_SECURE = True
 X_FRAME_OPTIONS = 'DENY'
 
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+EMAIL_HOST_USER = 'ezekieleyitayo2020@gmail.com'
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = 'curapets@gmail.com'
+
+SITE_DOMAIN = 'testdomain.com'
+SITE_PROTOCOL = 'https'
+
+
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
--- pet-backend/user/serializers.py
+++ pet-backend/user/serializers.py
@@ -126,6 +126,7 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
 
 class PasswordResetSerializer(serializers.Serializer):
     email = serializers.EmailField()
+    
 
     def validate_email(self, value):
         if not User.objects.filter(email=value).exists():
